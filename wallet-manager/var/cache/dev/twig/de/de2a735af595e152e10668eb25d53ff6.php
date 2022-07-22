864baa9532e131ce992bef8bfaa489c21c990da3
Fabien V <fabien.vermeiren@gmail.com>
2022-07-22 10:13:14 +0200

    modify the route

--- wallet-manager/var/cache/dev/twig/de/de2a735af595e152e10668eb25d53ff6.php
+++ wallet-manager/var/cache/dev/twig/de/de2a735af595e152e10668eb25d53ff6.php
@@ -27,10 +27,6 @@ class __TwigTemplate_a7c678640d1e19672b9469dbfe8c632c extends Template
         $this->parent = false;
 
         $this->blocks = [
-            'title' => [$this, 'block_title'],
-            'stylesheets' => [$this, 'block_stylesheets'],
-            'javascripts' => [$this, 'block_javascripts'],
-            'body' => [$this, 'block_body'],
         ];
     }
 
@@ -45,121 +41,121 @@ class __TwigTemplate_a7c678640d1e19672b9469dbfe8c632c extends Template
 
         // line 1
         echo "<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset=\"UTF-8\">
-        <title>";
-        // line 5
-        $this->displayBlock('title', $context, $blocks);
-        echo "</title>
-        <link rel=\"icon\" href=\"data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 128 128%22><text y=%221.2em%22 font-size=%2296%22>⚫️</text></svg>\">
-        ";
-        // line 8
-        echo "        ";
-        $this->displayBlock('stylesheets', $context, $blocks);
-        // line 11
-        echo "
-        ";
-        // line 12
-        $this->displayBlock('javascripts', $context, $blocks);
-        // line 15
-        echo "    </head>
-    <body>
-        ";
-        // line 17
-        $this->displayBlock('body', $context, $blocks);
-        // line 18
-        echo "    </body>
+<html lang=\"en\">
+  <head>
+    <meta charset=\"UTF-8\" />
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />
+    <meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\" />
+    <link rel=\"shortcut icon\" type=\"image/png\" href=\"/icon.png\" />
+
+    <link
+      href=\"https://fonts.googleapis.com/css?family=Poppins:400,500,600&display=swap\"
+      rel=\"stylesheet\"
+    />
+
+    <link rel=\"stylesheet\" href=\"style.css\" />
+    <title>My Wallet</title>
+  </head>
+  <body>
+    <!-- TOP NAVIGATION -->
+    <nav>
+      <p class=\"welcome\">Log in to get started</p>
+      <img src=\"logo.png\" alt=\"Logo\" class=\"logo\" />
+      <form class=\"login\">
+        <input
+          type=\"text\"
+          placeholder=\"user\"
+          class=\"login__input login__input--user\"
+        />
+        <!-- In practice, use type=\"password\" -->
+        <input
+          type=\"text\"
+          placeholder=\"PIN\"
+          maxlength=\"4\"
+          class=\"login__input login__input--pin\"
+        />
+        <button class=\"login__btn\">&rarr;</button>
+      </form>
+    </nav>
+
+    <main class=\"app\">
+      <!-- BALANCE -->
+      <div class=\"balance\">
+        <div>
+          <p class=\"balance__label\">Current balance</p>
+          <p class=\"balance__date\">
+            As of <span class=\"date\">05/03/2037</span>
+          </p>
+        </div>
+        <p class=\"balance__value\">0000€</p>
+      </div>
+
+      <!-- MOVEMENTS -->
+      <div class=\"movements\">
+        <div class=\"movements__row\">
+          <div class=\"movements__type movements__type--in\">in</div>
+          <div class=\"movements__date\">3 days ago</div>
+          <div class=\"movements__subject\">beers</div>
+          <div class=\"movements__value\">4 000€</div>
+        </div>
+        <div class=\"movements__row\">
+          <div class=\"movements__type movements__type--out\">
+            out
+          </div>
+          <div class=\"movements__date\">24/01/2037</div>
+          <div class=\"movements__subject\">maximum 20 characters</div>
+          <div class=\"movements__value\">-378€</div>
+        </div>
+      </div>
+
+      <!-- SUMMARY -->
+      <div class=\"summary\">
+        <p class=\"summary__label\">In</p>
+        <p class=\"summary__value summary__value--in\">0000€</p>
+        <p class=\"summary__label\">Out</p>
+        <p class=\"summary__value summary__value--out\">0000€</p>
+        <button class=\"btn--sort\">&downarrow; SORT</button>
+      </div>
+
+      <!-- OPERATION: income -->
+      <div class=\"operation operation--income\">
+        <h2>Add Income</h2>
+        <form class=\"form form--movement\">
+          <input type=\"number\" class=\"form__input form__input--movement-amount\" />
+          <input type=\"text\" class=\"form__input\" />
+          <button class=\"form__btn form__btn--movement\">&rarr;</button>
+          <label class=\"form__label form__label--movement\">Amount</label>
+          <label class=\"form__label\">Subject</label>
+        </form>
+      </div>
+
+      <!-- OPERATION: expense -->
+      <div class=\"operation operation--expense\">
+        <h2>Add Expense</h2>
+        <form class=\"form form--movement\">
+          <input type=\"number\" class=\"form__input form__input--movement-amount\" />
+          <input type=\"text\" class=\"form__input\" />
+          <button class=\"form__btn form__btn--movement\">&rarr;</button>
+          <label class=\"form__label form__label--movement\">Amount</label>
+          <label class=\"form__label\">Subject</label>
+        </form>
+      </div>
+
+      <!-- LOGOUT TIMER -->
+      <p class=\"logout-timer\">
+        You will be logged out in <span class=\"timer\">05:00</span>
+      </p>
+    </main>
+
+  </body>
 </html>
+
 ";
         
         $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
 
         
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
-
-    }
-
-    // line 5
-    public function block_title($context, array $blocks = [])
-    {
-        $macros = $this->macros;
-        $__internal_5a27a8ba21ca79b61932376b2fa922d2 = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
-        $__internal_5a27a8ba21ca79b61932376b2fa922d2->enter($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "title"));
-
-        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
-        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "title"));
-
-        echo "Welcome!";
-        
-        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
-
-        
-        $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
-
-    }
-
-    // line 8
-    public function block_stylesheets($context, array $blocks = [])
-    {
-        $macros = $this->macros;
-        $__internal_5a27a8ba21ca79b61932376b2fa922d2 = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
-        $__internal_5a27a8ba21ca79b61932376b2fa922d2->enter($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "stylesheets"));
-
-        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
-        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "stylesheets"));
-
-        // line 9
-        echo "            ";
-        echo twig_escape_filter($this->env, $this->env->getFunction('encore_entry_link_tags')->getCallable()("app"), "html", null, true);
-        echo "
-        ";
-        
-        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
-
-        
-        $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
-
-    }
-
-    // line 12
-    public function block_javascripts($context, array $blocks = [])
-    {
-        $macros = $this->macros;
-        $__internal_5a27a8ba21ca79b61932376b2fa922d2 = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
-        $__internal_5a27a8ba21ca79b61932376b2fa922d2->enter($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "javascripts"));
-
-        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
-        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "javascripts"));
-
-        // line 13
-        echo "            ";
-        echo twig_escape_filter($this->env, $this->env->getFunction('encore_entry_script_tags')->getCallable()("app"), "html", null, true);
-        echo "
-        ";
-        
-        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
-
-        
-        $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
-
-    }
-
-    // line 17
-    public function block_body($context, array $blocks = [])
-    {
-        $macros = $this->macros;
-        $__internal_5a27a8ba21ca79b61932376b2fa922d2 = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
-        $__internal_5a27a8ba21ca79b61932376b2fa922d2->enter($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
-
-        $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
-        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
-
-        
-        $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
-
-        
-        $__internal_5a27a8ba21ca79b61932376b2fa922d2->leave($__internal_5a27a8ba21ca79b61932376b2fa922d2_prof);
 
     }
 
@@ -170,30 +166,121 @@ class __TwigTemplate_a7c678640d1e19672b9469dbfe8c632c extends Template
 
     public function getDebugInfo()
     {
-        return array (  149 => 17,  136 => 13,  126 => 12,  113 => 9,  103 => 8,  84 => 5,  72 => 18,  70 => 17,  66 => 15,  64 => 12,  61 => 11,  58 => 8,  53 => 5,  47 => 1,);
+        return array (  43 => 1,);
     }
 
     public function getSourceContext()
     {
         return new Source("<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset=\"UTF-8\">
-        <title>{% block title %}Welcome!{% endblock %}</title>
-        <link rel=\"icon\" href=\"data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 128 128%22><text y=%221.2em%22 font-size=%2296%22>⚫️</text></svg>\">
-        {# Run `composer require symfony/webpack-encore-bundle` to start using Symfony UX #}
-        {% block stylesheets %}
-            {{ encore_entry_link_tags('app') }}
-        {% endblock %}
+<html lang=\"en\">
+  <head>
+    <meta charset=\"UTF-8\" />
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />
+    <meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\" />
+    <link rel=\"shortcut icon\" type=\"image/png\" href=\"/icon.png\" />
 
-        {% block javascripts %}
-            {{ encore_entry_script_tags('app') }}
-        {% endblock %}
-    </head>
-    <body>
-        {% block body %}{% endblock %}
-    </body>
+    <link
+      href=\"https://fonts.googleapis.com/css?family=Poppins:400,500,600&display=swap\"
+      rel=\"stylesheet\"
+    />
+
+    <link rel=\"stylesheet\" href=\"style.css\" />
+    <title>My Wallet</title>
+  </head>
+  <body>
+    <!-- TOP NAVIGATION -->
+    <nav>
+      <p class=\"welcome\">Log in to get started</p>
+      <img src=\"logo.png\" alt=\"Logo\" class=\"logo\" />
+      <form class=\"login\">
+        <input
+          type=\"text\"
+          placeholder=\"user\"
+          class=\"login__input login__input--user\"
+        />
+        <!-- In practice, use type=\"password\" -->
+        <input
+          type=\"text\"
+          placeholder=\"PIN\"
+          maxlength=\"4\"
+          class=\"login__input login__input--pin\"
+        />
+        <button class=\"login__btn\">&rarr;</button>
+      </form>
+    </nav>
+
+    <main class=\"app\">
+      <!-- BALANCE -->
+      <div class=\"balance\">
+        <div>
+          <p class=\"balance__label\">Current balance</p>
+          <p class=\"balance__date\">
+            As of <span class=\"date\">05/03/2037</span>
+          </p>
+        </div>
+        <p class=\"balance__value\">0000€</p>
+      </div>
+
+      <!-- MOVEMENTS -->
+      <div class=\"movements\">
+        <div class=\"movements__row\">
+          <div class=\"movements__type movements__type--in\">in</div>
+          <div class=\"movements__date\">3 days ago</div>
+          <div class=\"movements__subject\">beers</div>
+          <div class=\"movements__value\">4 000€</div>
+        </div>
+        <div class=\"movements__row\">
+          <div class=\"movements__type movements__type--out\">
+            out
+          </div>
+          <div class=\"movements__date\">24/01/2037</div>
+          <div class=\"movements__subject\">maximum 20 characters</div>
+          <div class=\"movements__value\">-378€</div>
+        </div>
+      </div>
+
+      <!-- SUMMARY -->
+      <div class=\"summary\">
+        <p class=\"summary__label\">In</p>
+        <p class=\"summary__value summary__value--in\">0000€</p>
+        <p class=\"summary__label\">Out</p>
+        <p class=\"summary__value summary__value--out\">0000€</p>
+        <button class=\"btn--sort\">&downarrow; SORT</button>
+      </div>
+
+      <!-- OPERATION: income -->
+      <div class=\"operation operation--income\">
+        <h2>Add Income</h2>
+        <form class=\"form form--movement\">
+          <input type=\"number\" class=\"form__input form__input--movement-amount\" />
+          <input type=\"text\" class=\"form__input\" />
+          <button class=\"form__btn form__btn--movement\">&rarr;</button>
+          <label class=\"form__label form__label--movement\">Amount</label>
+          <label class=\"form__label\">Subject</label>
+        </form>
+      </div>
+
+      <!-- OPERATION: expense -->
+      <div class=\"operation operation--expense\">
+        <h2>Add Expense</h2>
+        <form class=\"form form--movement\">
+          <input type=\"number\" class=\"form__input form__input--movement-amount\" />
+          <input type=\"text\" class=\"form__input\" />
+          <button class=\"form__btn form__btn--movement\">&rarr;</button>
+          <label class=\"form__label form__label--movement\">Amount</label>
+          <label class=\"form__label\">Subject</label>
+        </form>
+      </div>
+
+      <!-- LOGOUT TIMER -->
+      <p class=\"logout-timer\">
+        You will be logged out in <span class=\"timer\">05:00</span>
+      </p>
+    </main>
+
+  </body>
 </html>
+
 ", "base.html.twig", "/home/fabien/beCode/workshop-symfony-starter/wallet-manager/templates/base.html.twig");
     }
 }
